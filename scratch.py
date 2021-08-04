12bab34b8906ca4339df7d40d7301027753e6e99
Fred <itamar.fradkin@gmail.com>
2021-08-04 22:57:34 +0300

    experiment

---
+++ scratch.py
@@ -0,0 +1,19 @@
+import praw
+
+
+
+reddit = praw.Reddit()
+
+sub = 'wallstreetbets'
+
+subreddit = reddit.subreddit(sub)
+
+i = 1
+for post in subreddit.hot(limit=1):
+    print(post.id)
+    print(post.author)
+    print(post.title)
+    print(post.selftext)
+    print('---------------------------------------------------------------------------\n')
+    for comment in post.comments:
+        print(comment)
